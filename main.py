420ed4209cdcf38b79ec49eee4be5b59fb393ab7
Wei Ouyang <oeway007@gmail.com>
2025-08-05 17:27:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from hypha_rpc import api
 
-from some_module.test_import import this_function_does_nothing
-
 async def setup():
     print("hello")
-    this_function_does_nothing()
 
 
 api.export({
